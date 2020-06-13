acf27e9799d2920c22bbe9ba2a41a90b6efd68c3
viv-ishere <47481915+viv-ishere@users.noreply.github.com>
2020-06-13 14:03:36 -0400

    als;dkfj

--- src/Vector.java
+++ src/Vector.java
@@ -1,4 +1,43 @@
 
-public class Vector {
-
+public class Vector 
+{
+	int x;
+	int y;
+	
+	public Vector(int x, int y)
+	{
+		
+	}
+	
+	void addVector()
+	{
+		
+	}
+	
+	void subVector()
+	{
+		
+	}
+	
+	void divVector()
+	{
+		
+	}
+	
+	void multVector()
+	{
+		
+	}
+	
+	double returnMag()
+	{
+		double mag = 0;
+		return mag;
+	}
+	
+	void normalize()
+	{
+		
+	}
+	
 }
